1335c063595efea0d88d4d0099a183a8cc7d75d7
Atanas Stankin <a_stankin@abv.bg>
2023-04-24 22:38:05 +0300

    update

--- pass_data/pass_data_as_voltage/pass_data_analog_voltage.py
+++ pass_data/pass_data_as_voltage/pass_data_analog_voltage.py
@@ -10,13 +10,18 @@ arrow_ = arrow(length=arrow_length, shaftwidth=arrow_width, color=color.red, axi
 data = serial.Serial('COM10', 115200)
 time.sleep(1)
 
+box_X = 2.5
+box_Y = 1.5
+box_Z = 0.1
+my_case = box(color=color.white, size=vector(box_X, box_Y, box_Z), pos=vector(0, 0.9*box_Y/2, -box_Z/2))
+
 while True:
     while data.in_waiting == 0:
         pass
     data_packet = str(data.readline(), 'utf-8').strip('\r\n')
     pot_val = int(data_packet)
     voltage = (5/1023)*pot_val
-    theta = -2*np.pi/3069*pot_val + 5*np.pi/6
+    theta = -np.pi/1023*pot_val + np.pi
     arrow_.axis = vector(arrow_length * np.cos(theta), arrow_length * np.sin(theta), 0)
     # label.text = str(round(voltage, 1))
     # for theta in np.linspace(0, np.pi, 150):
